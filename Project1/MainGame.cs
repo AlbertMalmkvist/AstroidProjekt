9fdefd1710ad50dcef8ccdcc83d7c45e5d9c7d44
AlbertMalmkvist <48481758+AlbertMalmkvist@users.noreply.github.com>
2021-09-27 10:58:52 +0200

    Good enough

--- Project1/MainGame.cs
+++ Project1/MainGame.cs
@@ -2,31 +2,52 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace AstroidProjekt
 {
     public class MainGame : Game
     {
-        private GraphicsDeviceManager graphics;
-        private SpriteBatch spriteBatch;
-        public Texture2D Astroids;
-        public Texture2D Ships;
-
-        public Vector2 pos;
-        public Vector2 velocity;
-        public Vector2 ShipPos;
-
+        //All ints used through out the program
         public int ScrWidth;
         public int ScrHeight;
         public int score;
+        public int lives = 5;
+        public int Timer = 200;
 
+        //Created with the program
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        //All Texture2D used through out the program
+        public Texture2D Background;
+        public Texture2D Astroids;
+        public Texture2D Ships;
+        public Texture2D Lifes;
+        public Texture2D Boom;
+
+        //All Vector2 used through out the program
+        public Vector2 pos;
+        public Vector2 velocity;
+        public Vector2 LifePos;
+
+        // Mousestate
         MouseState mouseState, previousMouseState;
 
+        //Ranomisation
         Random rand;
 
         bool destroyed;
 
+        //Access to the other classes
         public Astroid astroid;
+        public Ship ship;
+
+        //Lists using the other classes
+        List<Astroid> AstroidList;
+        List<Ship> ShipList;
+
+        Rectangle BoomReact;
 
 
         public MainGame()
@@ -46,16 +67,26 @@ namespace AstroidProjekt
             IsMouseVisible = true;
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            //Getting the images used
             Astroids = Content.Load<Texture2D>("astroid");
             Ships = Content.Load<Texture2D>("spacecraft");
+            Lifes = Content.Load<Texture2D>("Lifes");
+            Background = Content.Load<Texture2D>("spaceFront");
+            Boom = Content.Load<Texture2D>("explosion");
+
             ScrWidth = Window.ClientBounds.Width;
             ScrHeight = Window.ClientBounds.Height;
 
             mouseState = Mouse.GetState();
 
+            AstroidList = new List<Astroid>();
+            ShipList = new List<Ship>();
+
             rand = new Random();
-            int YDirection = rand.Next(1, 3);
-            int XDirection = rand.Next(1, 3);
+
+            for (int a = 0; a <= 2; a++){
+            int YDirection = rand.Next(a + 1, a + 1);
+            int XDirection = rand.Next(a + 1, a + 1);
 
             int startY = ScrWidth + Astroids.Width;
             int startx = ScrWidth + Astroids.Height;
@@ -63,6 +94,48 @@ namespace AstroidProjekt
             Vector2 startpos = new Vector2(startY, startx);
             Vector2 direction = new Vector2(YDirection, XDirection);
             astroid = new Astroid(Astroids, startpos, direction, ScrWidth, ScrHeight);
+
+            AstroidList.Add(astroid);
+            }
+
+            for (int b = 0; b <= 5; b++){
+                int startY = ScrWidth + Ships.Width;
+                int startx = ScrWidth + Ships.Height;
+                if(b == 0){
+                Vector2 direction = new Vector2(1, 3);
+                Vector2 startpos = new Vector2(startY, startx);
+                ship = new Ship(Ships, startpos, direction, ScrWidth, ScrHeight);
+                ShipList.Add(ship);
+                }
+                else if (b == 1)
+                {
+                    Vector2 direction = new Vector2(3, -1);
+                    Vector2 startpos = new Vector2(startY, startx);
+                    ship = new Ship(Ships, startpos, direction, ScrWidth, ScrHeight);
+                    ShipList.Add(ship);
+                }
+                else if (b == 2)
+                {
+                    Vector2 direction = new Vector2(1, -3);
+                    Vector2 startpos = new Vector2(startY, startx);
+                    ship = new Ship(Ships, startpos, direction, ScrWidth, ScrHeight);
+                    ShipList.Add(ship);
+                }
+                else if (b == 3)
+                {
+                    Vector2 direction = new Vector2(-3, -1);
+                    Vector2 startpos = new Vector2(startY, startx);
+                    ship = new Ship(Ships, startpos, direction, ScrWidth, ScrHeight);
+                    ShipList.Add(ship);
+                }
+                else
+                {
+                    Vector2 direction = new Vector2(3, 1);
+                    Vector2 startpos = new Vector2(startY, startx);
+                    ship = new Ship(Ships, startpos, direction, ScrWidth, ScrHeight);
+                    ShipList.Add(ship);
+                }
+            }
         }
 
         protected override void Update(GameTime gameTime)
@@ -73,62 +146,135 @@ namespace AstroidProjekt
 
             previousMouseState = mouseState;
             mouseState = Mouse.GetState();
+            BoomReact = new Rectangle(mouseState.X - Boom.Width / 2, mouseState.Y - Boom.Height / 2, Boom.Width, Boom.Height);
 
             if (pos.X < 0 || pos.X > ScrWidth - Astroids.Width)
             {
                 velocity = velocity * -1;
             }
 
-            astroid.UpdateAstroid();
-            if (astroid.pos.Y < 0 - Astroids.Height && astroid.alive == true)
+            if (lives > 0)
             {
-                astroid.alive = false;
-            }
-            else if (astroid.popRect.Contains(mouseState.X, mouseState.Y))
-            {
-                if (mouseState.LeftButton == ButtonState.Pressed)
+                foreach (Astroid astroid in AstroidList)
                 {
-                    destroyed = astroid.IsAstroidDestroyed(mouseState.X, mouseState.Y);
-
-                    if (destroyed)
+                    astroid.UpdateAstroid();
+                    if (astroid.pos.Y < 0 - Astroids.Height && astroid.Active == true)
                     {
-                        score += 10;
+                        astroid.Active = false;
+                    }
+                    else if (astroid.popRect.Contains(mouseState.X, mouseState.Y))
+                    {
+                        if (mouseState.LeftButton == ButtonState.Pressed)
+                        {
+                            destroyed = astroid.IsAstroidDestroyed(mouseState.X, mouseState.Y);
+
+                            if (destroyed)
+                            {
+                                score += 10;
+                                Timer = 0;
+                                if (score == 1000)
+                                {
+                                    AstroidList.Clear();
+                                    ShipList.Clear();
+                                }
+                            }
+                        }
+                    }
+                    if (!astroid.Active)
+                    {
+                        AstroidList.Remove(astroid);
+
+                        int YDirection = rand.Next(-3, 3);
+                        int XDirection = rand.Next(-3, 3);
+                        if (YDirection == 0 || XDirection == 0)
+                        {
+                            XDirection = 4;
+                            YDirection = -4;
+                        }
+
+                        int startY = ScrWidth + Astroids.Width;
+                        int startx = ScrHeight + Astroids.Height;
+
+                        Vector2 startpos = new Vector2(startY, startx);
+                        Vector2 direction = new Vector2(YDirection, XDirection);
+                        Astroid Newastroid = new Astroid(Astroids, startpos, direction, ScrWidth, ScrHeight);
+
+                        AstroidList.Add(Newastroid);
+
+                        break;
                     }
                 }
-            }
 
-            if (!astroid.alive)
+                foreach (Ship ship in ShipList)
+                {
+                    {
+                        ship.UpdateShip();
+                        if (ship.pos.Y < 0 - Ships.Height && ship.Active == true)
+                        {
+                            ship.Active = false;
+                        }
+                        else if (ship.popRect.Contains(mouseState.X, mouseState.Y))
+                        {
+                            if (mouseState.LeftButton == ButtonState.Pressed)
+                            {
+                                destroyed = ship.IsShipDestroyed(mouseState.X, mouseState.Y);
+
+                                if (destroyed)
+                                {
+                                    lives--;
+                                    Timer = 0;
+                                }
+                            }
+                        }
+                        if (!ship.Active)
+                        {
+                            ShipList.Remove(ship);
+                            break;
+                        }
+                    }
+                }
+
+            }
+            else if (lives == 0)
             {
-                int YDirection = rand.Next(-3, 3);
-                int XDirection = rand.Next(-3, 3);
-
-                int startY = ScrWidth + Astroids.Width;
-                int startx = ScrHeight + Astroids.Height;
-
-                Vector2 startpos = new Vector2(startY, startx);
-                Vector2 direction = new Vector2(YDirection, XDirection);
-                astroid = new Astroid(Astroids, startpos, direction, ScrWidth, ScrHeight);
+                AstroidList.Clear();
+                ShipList.Clear();
             }
-
-            System.Diagnostics.Debug.WriteLine("score...." + score);
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-            ShipPos = Vector2.Zero;
+            LifePos = Vector2.Zero;
             spriteBatch.Begin();
+
+            spriteBatch.Draw(Background, Vector2.Zero, Color.White);
+
+            foreach(Astroid astroid in AstroidList){
             astroid.DrawAstroid(spriteBatch);
-            for(int i = 0; i < 5; i++)
-            {
-            spriteBatch.Draw(Ships, ShipPos, Color.White);
-                ShipPos.X = ShipPos.X + Ships.Width;
             }
-
-            Window.Title = "Astroids " + " Score: " + score;
+            foreach(Ship ship in ShipList)
+            {
+                ship.DrawShip(spriteBatch);
+                spriteBatch.Draw(Lifes, LifePos, Color.White);
+                LifePos.X = LifePos.X + Lifes.Width;
+            }
+            if (Timer < 200)
+            {
+                Timer += (int)gameTime.ElapsedGameTime.TotalMilliseconds;
+                spriteBatch.Draw(Boom, BoomReact, Color.White);
+            }
+            Window.Title = "Astroids " + " Score: " + score + "   Lives:" + lives;
+            if (score == 1000)
+            {
+                Window.Title = "Astroids " + "      YOU WIN";
+            }
+            if (lives == 0)
+            {
+                Window.Title = "Astroids " + "      YOU LOSE";
+            }
             spriteBatch.End();
-
             base.Draw(gameTime);
         }
     }
--- Project1/Ship.cs
+++ Project1/Ship.cs
@@ -7,9 +7,9 @@ using Microsoft.Xna.Framework.Input;
 
 namespace AstroidProjekt
 {
-    public class Astroid
+    public class Ship
     {
-        public Texture2D AstroidImg;
+        public Texture2D ShipImg;
 
         public Vector2 pos;
         public Vector2 velocity;
@@ -20,54 +20,54 @@ namespace AstroidProjekt
         public bool Active;
 
         public Rectangle popRect;
-        public Astroid(Texture2D AstroidImg, Vector2 pos, Vector2 velocity, int ScrWidth, int ScrHeight)
+        public Ship(Texture2D ShipImg, Vector2 pos, Vector2 velocity, int ScrWidth, int ScrHeight)
         {
-            this.AstroidImg = AstroidImg;
+            this.ShipImg = ShipImg;
             this.pos = pos;
             this.velocity = velocity;
             this.ScrWidth = ScrWidth;
             this.ScrHeight = ScrHeight;
-            
+
             Active = true;
         }
 
-        public void UpdateAstroid()
+        public void UpdateShip()
         {
             pos = pos + velocity;
 
-            float othersideX = AstroidImg.Width + AstroidImg.Width + ScrWidth;
-            float othersideY = AstroidImg.Height + AstroidImg.Height + ScrHeight;
+            float othersideX = ShipImg.Width + ShipImg.Width + ScrWidth;
+            float othersideY = ShipImg.Height + ShipImg.Height + ScrHeight;
 
-            if (pos.X > ScrWidth + AstroidImg.Width)
+            if (pos.X > ScrWidth + ShipImg.Width)
             {
                 pos.X = pos.X - othersideX;
             }
-            if (pos.X < 0 - AstroidImg.Width)
+            if (pos.X < 0 - ShipImg.Width)
             {
                 pos.X = pos.X + othersideX;
             }
 
-            if (pos.Y > ScrHeight + AstroidImg.Height)
+            if (pos.Y > ScrHeight + ShipImg.Height)
             {
                 pos.Y = pos.Y - othersideY;
             }
-            if (pos.Y < 0 - AstroidImg.Height)
+            if (pos.Y < 0 - ShipImg.Height)
             {
                 pos.Y = pos.Y + othersideY;
             }
             pos = pos + velocity;
-            popRect = new Rectangle((int)(pos.X), (int)(pos.Y), AstroidImg.Width, AstroidImg.Height);
+            popRect = new Rectangle((int)(pos.X), (int)(pos.Y), ShipImg.Width, ShipImg.Height);
         }
 
-        public void DrawAstroid(SpriteBatch spriteBatch)
+        public void DrawShip(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(AstroidImg, pos, Color.White);
+            spriteBatch.Draw(ShipImg, pos, Color.White);
         }
 
-        public bool IsAstroidDestroyed(int x, int y)
+        public bool IsShipDestroyed(int x, int y)
         {
             bool isDestroyed = false;
-            Rectangle rect = new Rectangle((int)(pos.X), (int)(pos.Y), AstroidImg.Width, AstroidImg.Height);
+            Rectangle rect = new Rectangle((int)(pos.X), (int)(pos.Y), ShipImg.Width, ShipImg.Height);
 
             if (rect.Contains(x, y))
             {
